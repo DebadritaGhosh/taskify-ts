28fbccfbeda266b5ab9237944edd80aab11e6adb
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2024-01-15 18:11:37 +0530

    added todolist components

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./style.css";
 
-function InputField() {
+interface Props{
+	todo: string,
+	setTodo: React.Dispatch<React.SetStateAction<string>>,
+	handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({todo,setTodo,handleAdd}: Props) => {
+	const inputRef = useRef<HTMLInputElement>(null);
+
+
 	return (
-		<form className='input'>
+		<form className='input' onSubmit={(e) =>{ 
+			inputRef.current?.blur();
+			handleAdd(e)
+			}}>
 			<input type='input' placeholder='Enter a task'
 				className='input__box'
+				onChange={(e) => setTodo(e.target.value)}
 			/>
 			<button className='input_submit'>Go</button>
 		</form>
