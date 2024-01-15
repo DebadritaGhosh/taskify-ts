75a586a37125b01896adca9788aa114821d2ac86
Debadrita Ghosh <debadrita.ghosh2010@gmail.com>
2024-01-15 17:34:01 +0530

    added InputField component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import InputField from './components/InputField';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
+      <span className='heading'>Taskify</span>
+      <InputField />
     </div>
   );
 }
